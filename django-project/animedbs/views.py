1b3b074925809e6b5246686a2f7ac5ce2dd1c0d4
Shaform <shaform@gmail.com>
2012-04-22 11:50:19 +0800

    Add basic construct for the website.

---
+++ django-project/animedbs/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def home(request):
+    return render_to_response('home.html',
+            context_instance=RequestContext(request))
