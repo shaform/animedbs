01c52e047c8950d039420631cde7bea3a33c6261
darkgerm <darkgerm@gmail.com>
2012-05-06 18:34:03 +0800

    add bootstrap files

--- django-project/animedbs/urls.py
+++ django-project/animedbs/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
         url(r'^$', 'animedbs.views.home'),
         url(r'^logout$', 'animedbs.views.logout'),
-        url(r'^user$', 'animedbs.views.users'),
+        url(r'^users$', 'animedbs.views.users'),
         url(r'^profile$', 'animedbs.views.profile'),
         url(r'^author$', 'animedbs.views.authors'),
         url(r'^anime$', 'animedbs.views.animes'),
